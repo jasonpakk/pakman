a8afc5b377e1c66960c4df199c6a4bcce4fb0e17
JY2452 <jaeyoung.pak8@gmail.com>
2025-02-05 19:13:06 -0500

    unset

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? "/pacman" : "",
-  assetPrefix: isProd ? "/pacman" : "",
-  output: "export",
-};
+const nextConfig = {};
 
 export default nextConfig;
