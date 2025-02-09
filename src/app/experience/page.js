b8acfd846d11ae6a456d9a3acf0de47276a2686e
JY2452 <jaeyoung.pak8@gmail.com>
2025-02-09 17:39:41 -0500

    add coding icons

--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -23,9 +23,42 @@ export default function Experience() {
       <div id="experienceContent">
         <img id="desk" src="/experience/desk.gif" alt="desk" />
         <img id="backAvatar" src="/avatar/back.png" alt="back" />
-        <img id="cabinet" src="/experience/cabinet.png" alt="cabinet" />
 
         <WorkExperience />
+
+        <div id="cabinetContainer">
+          <img id="cabinet" src="/experience/cabinet.png" alt="cabinet" />
+          <div id="cabinetTop">
+            <img id="" src="/experience/library.png" alt="my library" />
+            <img id="" src="/experience/aws.png" alt="aws" />
+          </div>
+          <div id="cabinetContent">
+            <div className="cabinetRow">
+              <img id="" src="/experience/java.png" alt="java" />
+              <img id="" src="/experience/html.png" alt="html" />
+              <img id="" src="/experience/css.png" alt="css" />
+              <img id="" src="/experience/js.png" alt="javascript" />
+            </div>
+            <div className="cabinetRow">
+              <img id="" src="/experience/c.png" alt="c" />
+              <img id="" src="/experience/csharp.png" alt="c#" />
+              <img id="" src="/experience/python.png" alt="python" />
+              <img id="" src="/experience/go.png" alt="golang" />
+            </div>
+            <div className="cabinetRow">
+              <img id="" src="/experience/ts.png" alt="typescript" />
+              <img id="" src="/experience/react.png" alt="react" />
+              <img id="" src="/experience/angular.png" alt="angular" />
+              <img id="" src="/experience/node.png" alt="node js" />
+            </div>
+            <div className="cabinetRow">
+              <img id="" src="/experience/unity.png" alt="unity" />
+              <img id="" src="/experience/mongo.png" alt="mongo" />
+              <img id="" src="/experience/docker.png" alt="docker" />
+              <img id="" src="/experience/figma.png" alt="figma" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
