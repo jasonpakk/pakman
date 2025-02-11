564809b6a20c22cd293aac7880cafec3c00aa016
JY2452 <jaeyoung.pak8@gmail.com>
2025-02-11 02:14:12 -0500

    add indiv proj pages

---
+++ src/app/projects/spacecolony/page.js
@@ -0,0 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function SpaceColony() {
+  const router = useRouter();
+
+  return (
+    <div className="page">
+      <div className="pageHeader" style={{ alignSelf: "flex-end" }}>
+        <button onClick={() => router.push("/projects")}>
+          <img src="/pacman/orange.png" alt="orange" />
+          <p>Back to Projects</p>
+        </button>
+      </div>
+
+      <h1>Project Name</h1>
+    </div>
+  );
+}
