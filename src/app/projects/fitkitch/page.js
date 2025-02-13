7a6adf25acc77843479564b887d1f6c0319a4598
JY2452 <jaeyoung.pak8@gmail.com>
2025-02-12 20:17:29 -0500

    fit kitch done

--- src/app/projects/fitkitch/page.js
+++ src/app/projects/fitkitch/page.js
@@ -24,7 +24,7 @@ export default function FitKitch() {
       <div className="projectHeader">
         <img src="/projects/fitkitch.png" alt="fitkitch" />
         <div className="text">
-          <h1>FITKITCH</h1>
+          <h1 style={{ color: "#9e5ebf" }}>FITKITCH</h1>
           <p>
             bereal but for fitness! complete daily workouts within the timeframe
             and work with your friends to reveal the surprise recipe
@@ -55,14 +55,14 @@ export default function FitKitch() {
           </div>
 
           <p style={{ marginTop: "1vw" }}>
-            as a programmer + musician, i wanted to integrate my two interests
-            into a single project.
+            asked by a professor at dartmouth to build a new app to motivate
+            young people to exercise using the benefits of cognitive health.
           </p>
           <br />
           <p>
-            senior year of high school, i partnered with my friend to design,
-            build, and program a xylophone-playing robot that can perform any
-            tune.
+            worked with 2 ui/ux designers, a PM, and 2 other developers at the
+            DALI Lab to design, develop, and deploy the app for a beta launch in
+            10 weeks.
           </p>
         </div>
       </div>
@@ -76,43 +76,40 @@ export default function FitKitch() {
 
           <ul>
             <li>
-              <strong>found:</strong> existing instrument-playing robots heavily
-              focus on musical technicality
+              <strong>formerly:</strong> professor came up with an idea for a
+              new app
               <ul>
-                <li>pitch accuracy, agility, precise timing, etc.</li>
+                <li>
+                  users play memory games after exercising to collect data on
+                  how exercise positively affects cognitive health
+                </li>
               </ul>
             </li>
             <li>
-              <strong>lacks:</strong> the expressive musicianship of a human
-              performer
+              <strong>we did our own research:</strong> found memory games were
+              not appealing for users after strenuous exercise
             </li>
             <li>
-              humans achieve <strong>musicality</strong> in ways such as:
+              <strong>new idea</strong>: inspired by the trending app, bereal
               <ul>
                 <li>
-                  <strong>dynamics</strong> - varying volume for emotional
-                  expression
+                  motivate young people to exercise through community obligation
+                  + personalized daily activity goals
                 </li>
                 <li>
-                  <strong>articulation</strong> - differences in clarity and
-                  attack of notes
+                  work <strong>together</strong> with friends towards a common
+                  goal
+                </li>
+                <li>
+                  fitwit becomes <strong>fitkitch</strong>
                 </li>
               </ul>
             </li>
           </ul>
         </div>
-        <div className="video">
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/9Ag7zkTR_XE?si=SE1SH0--3utFUcaT"
-            title="Bot-thoven Demo"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-          <p>watch bot-thoven perform popular classical tunes</p>
+        <div className="photo">
+          <img src="/projects/fitkitch/1.png" alt="fitkitch" />
+          <p>fitwit, the initial idea proposed by the client</p>
         </div>
       </div>
 
@@ -125,56 +122,157 @@ export default function FitKitch() {
 
           <ul>
             <li>
-              <strong>goal:</strong> build a robot that plays with human-like
-              musical expression
-            </li>
-            <li>
-              <strong>instrument of choice:</strong> xylophone
+              <strong>collaboration:</strong> leverage the power of community to
+              motivate users to exercise
               <ul>
                 <li>
-                  <strong>why?</strong>
-                  can easily strike keys with different force to achieve
-                  different musical effects
+                  inspired by <strong>cooking:</strong> an activity that brings
+                  people together to achieve a collective goal: a well-cooked,
+                  seasoned dish
+                </li>
+                <li>
+                  users create/join groups called "kitchens" with their friends
                 </li>
               </ul>
             </li>
             <li>
-              <strong>use:</strong> servo motors to strike xylophone keys
+              <strong>personalization:</strong> fitness goals tailored to the
+              user
               <ul>
-                <li>can control force of strike by varying the motor speed</li>
+                <li>
+                  each user has a daily fitness goal to complete, tailored to
+                  their fitness level and preferences
+                </li>
+                <li>integrated data from apple health kit</li>
               </ul>
             </li>
             <li>
-              <strong>code:</strong> in arduino / c++
+              <strong>mission:</strong> earn ingredients for a surprise recipe
               <ul>
                 <li>
-                  write program to collectively control servo motor based on
-                  music note input
+                  users who complete their daily goal earn an ingredient for
+                  their kitchen
+                </li>
+                <li>
+                  if the kitchen completes the mission, they unlock the overall
+                  surprise recipe!
                 </li>
               </ul>
             </li>
           </ul>
         </div>
         <div className="photo">
-          <img src="/projects/botthoven/1.png" alt="botthoven" />
-          <p>initial design plan</p>
+          <img
+            className="long"
+            src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzkybW40bWNwdmd3em9teTl0cmcwMmZwdm9qbXpob2FwMmxodmRlNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H8vnDy9YdgupWhFrl0/giphy.gif"
+            alt="fitkitch"
+          />
+          <p>presenting...fitkitch!</p>
         </div>
       </div>
 
       <div className="projectDetailRow">
         <div className="photo">
-          <img src="/projects/botthoven/2.png" alt="botthoven" />
-          <p>CAD mockup of servo-mallet system</p>
+          <img className="wide" src="/projects/fitkitch/2.png" alt="fitkitch" />
+          <p>sharing our project at a tech exhibition</p>
         </div>
-        <div className="photo">
-          <img src="/projects/botthoven/3.png" alt="botthoven" />
-          <p>3d printing servo stands</p>
-        </div>
-        <div className="photo">
-          <img src="/projects/botthoven/4.png" alt="botthoven" />
-          <p>finished servo-mallet system</p>
+        <div className="text">
+          <div className="title">
+            <img src="/icons/rightarrow.png" alt="rightarrow" />
+            <h2>DESIGN</h2>
+          </div>
+
+          <ul>
+            <li>
+              the design team created a clean, modern, and engaging interface
+            </li>
+            <li>
+              underwent multiple iterations to ensure the app was user-friendly,
+              and positively engaging
+            </li>
+            <li>
+              checkout the full design process at{" "}
+              <a
+                href="https://justin-chong.com/fitkitch-case-study"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="design portfolio"
+              >
+                our designer's portfolio here
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
+
+      <img
+        src="/projects/fitkitch/3.png"
+        alt="botthoven"
+        style={{
+          width: "70%",
+          alignSelf: "center",
+          marginTop: "5vw",
+          marginBottom: "5vw",
+        }}
+      />
+
+      <div className="projectDetailRow">
+        <div className="text">
+          <div className="title">
+            <img src="/icons/rightarrow.png" alt="rightarrow" />
+            <h2>DEVELOPMENT</h2>
+          </div>
+
+          <ul>
+            <li>
+              <strong>stack:</strong> mern
+            </li>
+
+            <li>
+              <strong>frontend:</strong> react native
+              <ul>
+                <li>
+                  create interactive user interface to app functionality,
+                  implementing onboarding, goal selection, kitchen formation,
+                  recipe presentation
+                </li>
+                <li>redux for state management</li>
+                <li>
+                  integration with apple healthkit data to track user's fitness
+                  progress
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>backend:</strong> node.js, express, mongodb
+              <ul>
+                <li>
+                  create restful api to handle authentication, kitchen creation,
+                  manage user information
+                </li>
+                <li>mongoose for mongodb object modeling</li>
+              </ul>
+            </li>
+            <li>
+              <strong>delivery:</strong> from design to release in 10 weeks
+              <ul>
+                <li>launched beta version for test users on testflight</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <img
+        src="/projects/fitkitch/4.png"
+        alt="fitkitch"
+        style={{
+          width: "70%",
+          alignSelf: "center",
+          marginTop: "2vw",
+          marginBottom: "5vw",
+        }}
+      />
 
       <p className="backtotop" onClick={() => handleScroll()}>
         ↑ back to top ↑
