a2d3418f91ce510cbe91362d3ffb18be365ca812
JY2452 <jaeyoung.pak8@gmail.com>
2025-02-21 18:48:44 -0500

    add more sound tracks

--- src/app/music/compositions.js
+++ src/app/music/compositions.js
@@ -21,105 +21,130 @@ export default function Compositions() {
   };
 
   return (
-    <div className="compositions">
-      <div id="header">
-        <h2>works list</h2>
-        <img src="/icons/music.png" alt="music" />
-      </div>
+    <>
+      <div className="compositions">
+        <div id="header">
+          <h2>arrangements</h2>
+          <img src="/icons/music.png" alt="music" />
+        </div>
 
-      <div id="content">
-        <div id="list">
-          {Object.keys(compositionsList).map((instrumentation) => (
-            <div className="section" key={instrumentation}>
-              <div className="sectionHeader">
-                <img src="/icons/rightarrow.png" alt="music" />
-                <p>for</p>
-                <h3>{instrumentation}</h3>
+        <div id="content">
+          <div id="list">
+            {Object.keys(compositionsList).map((instrumentation) => (
+              <div className="section" key={instrumentation}>
+                <div className="sectionHeader">
+                  <img src="/icons/rightarrow.png" alt="music" />
+                  <p>for</p>
+                  <h3>{instrumentation}</h3>
+                </div>
+                <ul>
+                  {compositionsList[instrumentation].map(
+                    (composition, index) => (
+                      <li
+                        key={index}
+                        onClick={() => setSelectedComposition(composition)}
+                        className={
+                          composition.title === selectedComposition.title &&
+                          composition.instrumentation ===
+                            selectedComposition.instrumentation
+                            ? "selected"
+                            : ""
+                        }
+                      >
+                        <p className="title">
+                          ♪&nbsp;&nbsp;&nbsp;{composition.title}
+                        </p>
+                        <p className="composer">{composition.composer}</p>
+                      </li>
+                    )
+                  )}
+                </ul>
               </div>
-              <ul>
-                {compositionsList[instrumentation].map((composition, index) => (
-                  <li
-                    key={index}
-                    onClick={() => setSelectedComposition(composition)}
-                    className={
-                      composition.title === selectedComposition.title &&
-                      composition.instrumentation ===
-                        selectedComposition.instrumentation
-                        ? "selected"
-                        : ""
-                    }
-                  >
-                    <p className="title">
-                      ♪&nbsp;&nbsp;&nbsp;{composition.title}
-                    </p>
-                    <p className="composer">{composition.composer}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-
-        <div id="preview">
-          <img id="note1" className="note" src="/icons/note.png" alt="music" />
-          <img id="note2" className="note" src="/icons/note.png" alt="music" />
-          <div className="text">
-            <h3>{selectedComposition.title}</h3>
-            <p className="subtext">{selectedComposition.subtext}</p>
-            <div id="composer">
-              <img src="/icons/person.png" alt="composer" />
-              <p>{selectedComposition.composer}</p>
-            </div>
-          </div>
-          <div
-            id="samplePage"
-            onClick={() => openModal(selectedComposition.samplePage)}
-          >
-            <img src={selectedComposition.samplePage} alt="music" />
-            <p>sample page</p>
+            ))}
           </div>
 
-          {selectedComposition.watch && (
-            <a
-              href={selectedComposition.watch}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="watch video"
-              className="listen available"
+          <div id="preview">
+            <img
+              id="note1"
+              className="note"
+              src="/icons/note.png"
+              alt="music"
+            />
+            <img
+              id="note2"
+              className="note"
+              src="/icons/note.png"
+              alt="music"
+            />
+            <div className="text">
+              <h3>{selectedComposition.title}</h3>
+              <p className="subtext">{selectedComposition.subtext}</p>
+              <div id="composer">
+                <img src="/icons/person.png" alt="composer" />
+                <p>{selectedComposition.composer}</p>
+              </div>
+            </div>
+            <div
+              id="samplePage"
+              onClick={() => openModal(selectedComposition.samplePage)}
             >
-              <img src="/icons/play.png" alt="music" />
-              <p>watch</p>
-            </a>
-          )}
-
-          {selectedComposition.listen && (
-            <SoundCloudPlayer trackUrl={selectedComposition.listen} />
-          )}
-
-          {!selectedComposition.watch && !selectedComposition.listen && (
-            <div className="listen">
-              <img src="/icons/play.png" alt="music" />
-              <p>coming soon</p>
+              <img src={selectedComposition.samplePage} alt="music" />
+              <p>sample page</p>
             </div>
-          )}
-          <div id="instrumentation">
-            <img src="/bio/viola.png" alt="instrumentation" />
-            <p style={{ fontWeight: "100" }}>for</p>
-            <p>{selectedComposition.instrumentation}</p>
-          </div>
-        </div>
-      </div>
 
-      {modalOpen && (
-        <div className="modal" onClick={closeModal}>
-          <span className="close" onClick={closeModal}>
-            &times;
-          </span>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <img src={modalImage} alt="sample page enlarged" />
+            {selectedComposition.watch && (
+              <a
+                href={selectedComposition.watch}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="watch video"
+                className="listen available"
+              >
+                <img src="/icons/play.png" alt="music" />
+                <p>watch</p>
+              </a>
+            )}
+
+            {selectedComposition.listen && (
+              <SoundCloudPlayer trackUrl={selectedComposition.listen} />
+            )}
+
+            {!selectedComposition.watch && !selectedComposition.listen && (
+              <div className="listen">
+                <img src="/icons/play.png" alt="music" />
+                <p>coming soon</p>
+              </div>
+            )}
+            <div id="instrumentation">
+              <img src="/bio/viola.png" alt="instrumentation" />
+              <p style={{ fontWeight: "100" }}>for</p>
+              <p>{selectedComposition.instrumentation}</p>
+            </div>
           </div>
         </div>
-      )}
-    </div>
+
+        {modalOpen && (
+          <div className="modal" onClick={closeModal}>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+              <img src={modalImage} alt="sample page enlarged" />
+            </div>
+          </div>
+        )}
+      </div>
+      <p id="contactText">
+        <a
+          href="mailto:jaeyoung.pak8@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="email"
+        >
+          contact me
+        </a>{" "}
+        for any inquiries
+      </p>
+    </>
   );
 }
--- src/app/music/data.js
+++ src/app/music/data.js
@@ -15,6 +15,8 @@ const compositionsList = {
       composer: "johannes brahms",
       samplePage: "/music/sample/brahms_pt.png",
       instrumentation: "PIANO, VIOLA, CELLO",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699060%3Fsecret_token%3Ds-lb3WbZQWRgG",
     },
   ],
 
@@ -47,7 +49,7 @@ const compositionsList = {
       samplePage: "/music/sample/christmas_festival.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https%3A//api.soundcloud.com/tracks/2035635104%3Fsecret_token%3Ds-ycwmo99oMGk",
+        "https%3A//api.soundcloud.com/tracks/2039657932%3Fsecret_token%3Ds-8KENYM9vO5b",
     },
     {
       title: "ah! vous dirai-je, maman!",
@@ -56,7 +58,7 @@ const compositionsList = {
       samplePage: "/music/sample/maman3.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https%3A//api.soundcloud.com/tracks/2035638016%3Fsecret_token%3Ds-SCZQLEXsF42",
+        "https%3A//api.soundcloud.com/tracks/2039657960%3Fsecret_token%3Ds-k8yXnnJ2p2d",
     },
     {
       title: "all i want for christmas is you",
@@ -104,7 +106,7 @@ const compositionsList = {
       samplePage: "/music/sample/concerto_2_violins.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https%3A//api.soundcloud.com/tracks/2035642200%3Fsecret_token%3Ds-ICA6KGmqXiF",
+        "https%3A//api.soundcloud.com/tracks/2039657940%3Fsecret_token%3Ds-1tKoKoztIY5",
     },
 
     {
@@ -115,7 +117,7 @@ const compositionsList = {
       samplePage: "/music/sample/danny3.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https%3A//api.soundcloud.com/tracks/2035636040%3Fsecret_token%3Ds-NE8AaAVJfLN",
+        "https%3A//api.soundcloud.com/tracks/2039657925%3Fsecret_token%3Ds-i1vAhntc8Kb",
     },
     {
       title: "disney medley",
@@ -142,6 +144,8 @@ const compositionsList = {
       subtext: "medley of the two christmas classics",
       samplePage: "/music/sample/white_christmas.png",
       instrumentation: "3 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039657944%3Fsecret_token%3Ds-6J8279egd9P",
     },
     {
       title: "hoe down",
@@ -149,6 +153,8 @@ const compositionsList = {
       composer: "aaron copland",
       samplePage: "/music/sample/hoe_down.png",
       instrumentation: "3 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039657956%3Fsecret_token%3Ds-0O6uVRmfNPo",
     },
     {
       title: "it's the most wonderful time of the year",
@@ -156,7 +162,7 @@ const compositionsList = {
       samplePage: "/music/sample/most_wonderful.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https://api.soundcloud.com/tracks/2035643360%3Fsecret_token%3Ds-0Y9SBpMIdEZ",
+        "https%3A//api.soundcloud.com/tracks/2039657952%3Fsecret_token%3Ds-WkuZcbxerR1",
     },
     {
       title: "john williams medley",
@@ -209,6 +215,8 @@ const compositionsList = {
       composer: "holiday tune",
       samplePage: "/music/sample/o_christmas_tree.png",
       instrumentation: "3 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039657948%3Fsecret_token%3Ds-AaONjGATVLM",
     },
 
     {
@@ -216,6 +224,8 @@ const compositionsList = {
       composer: "gabriel fauré",
       samplePage: "/music/sample/pavane.png",
       instrumentation: "3 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039657929%3Fsecret_token%3Ds-zK9fXiXPMOO",
     },
     {
       title: "presto from “summer”",
@@ -232,6 +242,8 @@ const compositionsList = {
       composer: "antonín dvořák",
       samplePage: "/music/sample/rondo_american.png",
       instrumentation: "3 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039657936%3Fsecret_token%3Ds-WAApOT11X9P",
     },
     {
       title: "rudolph the red-nosed reindeer",
@@ -255,7 +267,7 @@ const compositionsList = {
       samplePage: "/music/sample/silent_night.png",
       instrumentation: "3 VIOLAS",
       listen:
-        "https%3A//api.soundcloud.com/tracks/2035639220%3Fsecret_token%3Ds-5z7wNfJbn2U",
+        "https%3A//api.soundcloud.com/tracks/2039657928%3Fsecret_token%3Ds-78LFz3CYPHa",
     },
     {
       title: "sleigh ride",
@@ -292,6 +304,8 @@ const compositionsList = {
       composer: "charles dancla",
       samplePage: "/music/sample/maman4.png",
       instrumentation: "4 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699052%3Fsecret_token%3Ds-Ht8ER8NELkc",
     },
     {
       title: "chopsticks variations",
@@ -300,6 +314,8 @@ const compositionsList = {
       composer: "classic tune",
       samplePage: "/music/sample/chopsticks.png",
       instrumentation: "4 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699044%3Fsecret_token%3Ds-LQOpc8FXQ8W",
     },
     {
       title: "zowie! goes the weasel",
@@ -307,6 +323,8 @@ const compositionsList = {
       composer: "y. dobon",
       samplePage: "/music/sample/zowie4.png",
       instrumentation: "4 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699056%3Fsecret_token%3Ds-i2klXfm8J9R",
     },
   ],
 
@@ -318,6 +336,8 @@ const compositionsList = {
       composer: "traditional",
       samplePage: "/music/sample/danny8.png",
       instrumentation: "8 VIOLAS",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699048%3Fsecret_token%3Ds-DRwlcKbjODE",
     },
   ],
 
@@ -340,6 +360,8 @@ const compositionsList = {
       subtext: "popular soundtrack from a k-drama",
       samplePage: "/music/sample/b_rossette_w.png",
       instrumentation: "FLUTE, CLARINET, VIOLIN, VIOLA",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699041%3Fsecret_token%3Ds-O6ydca8EzjD",
     },
     {
       title: "tarantella",
@@ -347,6 +369,8 @@ const compositionsList = {
       composer: "camille saint-saëns",
       samplePage: "/music/sample/tarantella.png",
       instrumentation: "FLUTE, CLARINET, VIOLIN, VIOLA",
+      listen:
+        "https%3A//api.soundcloud.com/tracks/2039699040%3Fsecret_token%3Ds-xt9kPcM7WRK",
     },
   ],
 };
